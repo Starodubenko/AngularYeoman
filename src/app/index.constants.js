ea250efbee4991a3ecee6fa2d1a49588aae4ec6a
Rody <Rody10@mail.ru>
2016-02-04 20:11:39 +0600

    Initial commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('yeomanGetingStart')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
